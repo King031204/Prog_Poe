53b54b3f3b9e7d22331f32eb6b11801d220dbbe7
King031204 <161839488+King031204@users.noreply.github.com>
2024-04-12 23:33:16 +0200

    Update Recipe.cs

--- Recipe.cs
+++ Recipe.cs
@@ -1,96 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Prog_Poe
 {
     class Recipe
     {
-        public int RecipeNumber { get; set; }
-        public string[] NameOfRecipe { get; set; }
-        public int NumberOfIngredients { get; set; }
-        public string[] Ingredients { get; set; }
-        public double[] IngredientQuantities { get; set; }
-        public string[] UnitOfMeasurement { get; set; }
+        public Ingredients[] IngredientList { get; set; }
         public int NumberOfSteps { get; set; }
         public string[] Steps { get; set; }
 
-        public Recipe()
+        public Recipe(Ingredients[] ingredients, int numberOfSteps, string[] steps)
         {
-            RecipeNumber = 0;
-            NameOfRecipe = new string[0];
-            NumberOfIngredients = 0;
-            Ingredients = new string[0];
-            IngredientQuantities = new double[0];
-            UnitOfMeasurement = new string[0];
-            NumberOfSteps = 0;
-            Steps = new string[0];
-        }
-
-        public Recipe(int recipeNumber,string[] nameOfRecipe,int numberOfIngredients, string[] ingredients, double[] ingredientQuantities, string[] unitOfMeasurement, int numberOfSteps, string[] steps)
-        {
-            RecipeNumber = recipeNumber;
-            NameOfRecipe = nameOfRecipe;
-            NumberOfIngredients = numberOfIngredients;
-            Ingredients = ingredients;
-            IngredientQuantities = ingredientQuantities;
-            UnitOfMeasurement = unitOfMeasurement;
+            IngredientList = ingredients;
             NumberOfSteps = numberOfSteps;
             Steps = steps;
-
-        }
-        public void Scale(double factor)
-        {
-            for (int i = 0; i < UnitOfMeasurement.Length; i++)
-            {
-                IngredientQuantities[i] *= factor;
-            }
-        }
-        public void ResetIngredientQuantities()
-        {
-            // Quantities will be reset to their original values (should said values be stored in an array)
-
         }
 
         public void Display()
         {
+           
 
-            Console.WriteLine("Recipe:");
-            for (int i = 0; i < NameOfRecipe.Length; i++)
-            {
-                Console.WriteLine((i + 1) + ". " + NameOfRecipe[i]);
-            }
             Console.WriteLine("Ingredients:");
-            for (int i = 0; i < Ingredients.Length; i++)
+            foreach (Ingredients ingredient in IngredientList)
             {
-                Console.WriteLine(IngredientQuantities[i] + " " + UnitOfMeasurement[i] + " of " + Ingredients[i]);
+                ingredient.Display();
             }
 
-            Console.WriteLine("Steps:");
-            for (int i = 0; i < Steps.Length; i++)
+            Console.WriteLine("\nSteps:");
+            for (int i = 0; i < NumberOfSteps; i++)
             {
-                Console.WriteLine((i + 1) + ". " + Steps[i]);
+                Console.WriteLine($"{i + 1}. {Steps[i]}");
             }
         }
 
-        public void Clear()
+        public void Scale(double scalingFactor)
         {
-            RecipeNumber = 0;
-            NameOfRecipe = new string[0];
-            NumberOfIngredients = 0;
-            Ingredients = new string[0];
-            IngredientQuantities = new double[0];
-            UnitOfMeasurement = new string[0];
-            NumberOfSteps = 0;
-            Steps = new string[0];
+            foreach (Ingredients ingredient in IngredientList)
+            {
+                ingredient.Quantity *= scalingFactor;
+            }
         }
 
-        public void Reset()
+        public void ResetIngredientQuantities()
         {
-            Clear();
-            // Reset any other variables or states
+            foreach (Ingredients ingredient in IngredientList)
+            {
+                ingredient.Quantity = 0;
+            }
         }
     }
 }
