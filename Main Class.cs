6c3216a4fcff0076ed29accfb5b64ec249e531b4
King031204 <161839488+King031204@users.noreply.github.com>
2024-04-12 23:30:31 +0200

    Update Main Class.cs
    main class code was rewritten

--- Main Class.cs
+++ Main Class.cs
@@ -1,159 +1,157 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Prog_Poe
 {
-    internal class Class2
+    internal class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(
-                "************************************************************************************************************************" + "\n" +
-                "                                                   Welcome To Recipe Haven" + "\n" +
-                "************************************************************************************************************************");
-            Recipe[] recipes = new Recipe[0];
+            Recipe[] recipes = new Recipe[100];
             int recipeCount = 0;
 
             while (true)
             {
-                //Get Input for Number of Recipes
-                Console.Write("Enter the number of Recipes: ");
-                int RecipeNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("=================================");
+                Console.WriteLine("Welcome To Recipe Haven");
+                Console.WriteLine("=================================");
 
-                string[] nameOfRecipe = new string[RecipeNumber];
+                Console.WriteLine("1. Add Recipe");
+                Console.WriteLine("2. Show Saved Recipe");
+                Console.WriteLine("3. Change Recipe Scale");
+                Console.WriteLine("4. Reset Recipe Quantities");
+                Console.WriteLine("5. Remove All Saved Recipes");
+                Console.WriteLine("6. Close");
+                Console.WriteLine("=================================");
 
-                for (int i = 0; i < RecipeNumber; i++)
+                Console.Write("Input Selection: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
                 {
-                    //Get Name of Recipe
-                    Console.WriteLine($"Enter the name of the Recipe {i + 1}: ");
-#pragma warning disable CS8601 // Possible null reference assignment.
-                    nameOfRecipe[i] = Console.ReadLine();
-#pragma warning restore CS8601 // Possible null reference assignment.
+                    case 1:
+                        recipes[recipeCount] = EnterRecipe();
+                        recipeCount++;
+                        break;
+                    case 2:
+                        DisplayRecipes(recipes, recipeCount);
+                        break;
+                    case 3:
+                        ScaleRecipe(recipes, recipeCount);
+                        break;
+                    case 4:
+                        ResetQuantities(recipes, recipeCount);
+                        break;
+                    case 5:
+                        ClearAllData(recipes, ref recipeCount);
+                        break;
+                    case 6:
+                        Console.WriteLine("Thank you for using Recipe Haven!");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
                 }
-                //Get Input for Number of Ingredients
-                Console.Write("Enter the number of ingredients: ");
-                int NumberOfIngredients = Convert.ToInt32(Console.ReadLine());
+            }
+        }
 
-                string[] ingredients = new string[NumberOfIngredients];
-                double[] ingredientQuantities = new double[NumberOfIngredients];
-                string[] unitOfMeasurement = new string[NumberOfIngredients];
+        public static Recipe EnterRecipe()
+        {
 
-                for (int i = 0; i < NumberOfIngredients; i++)
+            Console.WriteLine("Enter the number of ingredients: ");
+            int numberOfIngredients = Convert.ToInt32(Console.ReadLine());
+
+            Ingredients[] ingredientList = new Ingredients[numberOfIngredients];
+
+            for (int i = 0; i < numberOfIngredients; i++)
+            {
+                Console.WriteLine($"Enter the name of Ingredient {i + 1}: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine($"Enter the Unit of Measurement of Ingredient {i + 1}: ");
+                string unitOfMeasurement = Console.ReadLine();
+
+                Console.WriteLine($"Enter the Quantity for Ingredient {i + 1}: ");
+                double quantity = Convert.ToDouble(Console.ReadLine());
+
+                ingredientList[i] = new Ingredients(name, quantity, unitOfMeasurement);
+            }
+
+            Console.WriteLine("Enter the number of Steps: ");
+            int numberOfSteps = Convert.ToInt32(Console.ReadLine());
+
+            string[] steps = new string[numberOfSteps];
+
+            for (int s = 0; s < numberOfSteps; s++)
+            {
+                Console.WriteLine($"Enter the Description of Step {s + 1}: ");
+                steps[s] = Console.ReadLine();
+            }
+
+            return new Recipe(ingredientList, numberOfSteps, steps);
+        }
+
+        public static void DisplayRecipes(Recipe[] recipes, int recipeCount)
+        {
+            if (recipeCount == 0)
+            {
+                Console.WriteLine("No recipe saved. Input recipe first.");
+            }
+            else
+            {
+                Console.WriteLine("All Recipes:");
+                for (int i = 0; i < recipeCount; i++)
                 {
-                    //Get Name of Ingredients
-                    Console.WriteLine($"Enter the name of Ingredient {i + 1}: ");
-#pragma warning disable CS8601 // Possible null reference assignment.
-                    ingredients[i] = Console.ReadLine();
-#pragma warning restore CS8601 // Possible null reference assignment.
-
-                    //Get Unit of Measurements for Ingredients
-                    Console.WriteLine($"Enter the Unit of Measurement of Ingredient {i + 1}: ");
-#pragma warning disable CS8601 // Possible null reference assignment.
-                    unitOfMeasurement[i] = Console.ReadLine();
-#pragma warning restore CS8601 // Possible null reference assignment.
-
-                    //Get Quantity of Ingredients
-                    Console.WriteLine($"Enter the Quantity for Ingredient {i + 1}: ");
-                    ingredientQuantities[i] = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Recipe {i + 1}:");
+                    recipes[i].Display();
                 }
+            }
+        }
 
-                // Get input for the Number of Steps
-                Console.WriteLine("Enter the number of Steps: ");
-                int numberOfSteps = Convert.ToInt32(Console.ReadLine());
+        public static void ScaleRecipe(Recipe[] recipes, int recipeCount)
+        {
+            Console.Write("Enter the recipe number to scale: ");
+            int recipeNumber = Convert.ToInt32(Console.ReadLine()) - 1;
 
-                string[] steps = new string[numberOfSteps];
-
-                for (int s = 0; s < numberOfSteps; s++)
-                {
-                    // Get Description of Steps
-                    Console.WriteLine($"Enter the Description of Step {s + 1}: ");
-#pragma warning disable CS8601 // Possible null reference assignment.
-                    steps[s] = Console.ReadLine();
-#pragma warning restore CS8601 // Possible null reference assignment.
-                }
-
-                // Create a new recipe object with the provided ingredients and steps
-                Recipe recipe = new Recipe(RecipeNumber,nameOfRecipe,NumberOfIngredients, ingredients, ingredientQuantities, unitOfMeasurement, numberOfSteps, steps);
-                Console.WriteLine("Recipe:");
-                recipe.Display();
-
-
-                Console.WriteLine("Enter Scaling Factor (0.5, 2, or 3) or enter 'reset' to reset the Quantities : ");
-
+            if (recipeNumber >= 0 && recipeNumber < recipeCount)
+            {
+                Console.WriteLine("Enter Scaling Factor (0.5, 2, or 3): ");
                 double scalingFactor = Convert.ToDouble(Console.ReadLine());
 
-                // Scale the ingredient quantities
-                for (int i = 0; i < NumberOfIngredients; i++)
-                {
-                    ingredientQuantities[i] *= scalingFactor;
-                }
-
-                Recipe scaledRecipe = new Recipe(RecipeNumber,nameOfRecipe,NumberOfIngredients, ingredients, ingredientQuantities, unitOfMeasurement, numberOfSteps, steps);
-
+                recipes[recipeNumber].Scale(scalingFactor);
                 Console.WriteLine("Scaled Recipe:");
-                scaledRecipe.Display();
-
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-                string input = Console.ReadLine();
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
-                if (input == "0.5")
-                {
-                    double factor = Convert.ToDouble(input);
-                    recipe.Scale(factor);
-                }
-                if (input == "2")
-                {
-                    double factor = Convert.ToDouble(input);
-                    recipe.Scale(factor);
-                }
-                if (input == "3")
-                {
-                    double factor = Convert.ToDouble(input);
-                    recipe.Scale(factor);
-                }
-                else if (input == "reset")
-                {
-                    recipe.ResetIngredientQuantities();
-                    recipe.Display();
-                }
-                Console.WriteLine("Enter 'clear' to clear all data(If data is cleared you will be able to input a new recipe), otherwise " + "\n" +
-                    "Enter display' to show all recipes, or 'stop' to end off session: ");
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-                input = Console.ReadLine();
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
-
-
-
-                if (input == "clear")
-                {
-                    recipe.Clear();
-                    recipeCount = 0;
-                }
-                else if (input == "display")
-                {
-                    Console.WriteLine("All Recipes:");
-                    foreach (Recipe r in recipes)
-                    {
-                        r.Display();
-                    }
-                }
-
-                if (input == "stop")
-                {
-                    Console.WriteLine("Thank you for using Recipe Haven!");
-                    break;//exit loop
-                }
-                else
-                {
-                    recipe.Reset();
-                }
-                recipes[recipeCount] = scaledRecipe;
-                recipeCount++;// Add the recipe to the array
+                recipes[recipeNumber].Display();
             }
+            else
+            {
+                Console.WriteLine("Invalid recipe number.");
+            }
+        }
+
+        public static void ResetQuantities(Recipe[] recipes, int recipeCount)
+        {
+            Console.Write("Enter the recipe number to reset quantities: ");
+            int recipeNumber = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            if (recipeNumber >= 0 && recipeNumber < recipeCount)
+            {
+                recipes[recipeNumber].ResetIngredientQuantities();
+                Console.WriteLine("Quantities reset for Recipe " + (recipeNumber + 1));
+                recipes[recipeNumber].Display();
+            }
+            else
+            {
+                Console.WriteLine("Invalid recipe number.");
+            }
+        }
+
+        public static void ClearAllData(Recipe[] recipes, ref int recipeCount)
+        {
+            Console.WriteLine("All data cleared.");
+            for (int i = 0; i < recipeCount; i++)
+            {
+                recipes[i] = null;
+            }
+            recipeCount = 0;
         }
     }
 }
-
